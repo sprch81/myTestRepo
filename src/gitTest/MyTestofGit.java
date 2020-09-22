35bdb3b0b96b82c749638b1188df7d1535df3162
sprch81 <cspreddy81@gmail.com>
2020-09-22 16:55:05 +1000

    my self

--- src/gitTest/MyTestofGit.java
+++ src/gitTest/MyTestofGit.java
@@ -4,6 +4,9 @@ public class MyTestofGit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("its working");
+		System.out.println("update");
+		
 
 	}
 
